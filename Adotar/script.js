c40767d03be2e2414aaff56019d473d1579557bb
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-03-06 00:22:08 -0300

    Script

--- Adotar/script.js
+++ Adotar/script.js
@@ -20,3 +20,103 @@ function show(nome) {
     }
   }
   
+  function showSelects() {
+    var animalType = document.getElementById("selectAnimal").value;
+    var selectGato = document.getElementById("selectCat");
+    var selectDog = document.getElementById("selectDog");
+    var selectBunny = document.getElementById("selectBunny");
+  
+    selectGato.style.display = "none";
+    selectDog.style.display = "none";
+    selectBunny.style.display = "none";
+  
+    if (animalType === "gato") {
+      selectGato.style.display = "block";
+    } else if (animalType === "cachorro") {
+      selectDog.style.display = "block";
+    } else if (animalType === "coelho") {
+      selectBunny.style.display = "block";
+    }
+  }
+
+  function showAnimalInfo() {
+    var div = document.getElementById("Conferir");
+    div.style.display = "block";
+
+    var selectGato = document.getElementById("selectCat");
+    var selectCachorro = document.getElementById("selectDog");
+    var selectCoelho = document.getElementById("selectBunny");
+
+    switch (selectGato.value) {
+        case "Bob":
+            document.getElementById("tipo-animal").textContent = "Gato";
+            document.getElementById("nome-animal").textContent = "Bob";
+            document.getElementById("idade-animal").textContent = "3 anos";
+            document.getElementById("sexo-animal").textContent = "Macho";
+            break;
+
+        case "Stela":
+            document.getElementById("tipo-animal").textContent = "Gato";
+            document.getElementById("nome-animal").textContent = "Stela";
+            document.getElementById("idade-animal").textContent = "2 anos";
+            document.getElementById("sexo-animal").textContent = "Fêmea";
+            break;
+
+        case "Scar":
+            document.getElementById("tipo-animal").textContent = "Gato";
+            document.getElementById("nome-animal").textContent = "Scar";
+            document.getElementById("idade-animal").textContent = "4 anos";
+            document.getElementById("sexo-animal").textContent = "Macho";
+            break;
+    }
+
+    switch (selectCachorro.value) {
+        case "Mel":
+            document.getElementById("tipo-animal").textContent = "Cachorro";
+            document.getElementById("nome-animal").textContent = "Mel";
+            document.getElementById("idade-animal").textContent = "2 anos";
+            document.getElementById("sexo-animal").textContent = "Fêmea";
+            break;
+
+        case "Mikasa":
+            document.getElementById("tipo-animal").textContent = "Cachorro";
+            document.getElementById("nome-animal").textContent = "Mikasa";
+            document.getElementById("idade-animal").textContent = "1 ano";
+            document.getElementById("sexo-animal").textContent = "Fêmea";
+            break;
+
+        case "Tim":
+            document.getElementById("tipo-animal").textContent = "Cachorro";
+            document.getElementById("nome-animal").textContent = "Tim";
+            document.getElementById("idade-animal").textContent = "3 anos";
+            document.getElementById("sexo-animal").textContent = "Macho";
+            break;
+    }
+
+    switch (selectCoelho.value) {
+        case "Matt":
+            document.getElementById("tipo-animal").textContent = "Coelho";
+            document.getElementById("nome-animal").textContent = "Matt";
+            document.getElementById("idade-animal").textContent = "1 ano";
+            document.getElementById("sexo-animal").textContent = "Macho";
+            break;
+
+        case "Lila":
+            document.getElementById("tipo-animal").textContent = "Coelho";
+            document.getElementById("nome-animal").textContent = "Lila";
+            document.getElementById("idade-animal").textContent = "2 anos";
+            document.getElementById("sexo-animal").textContent = "Fêmea";
+            break;
+
+        case "Jude":
+            document.getElementById("tipo-animal").textContent = "Coelho";
+            document.getElementById("nome-animal").textContent = "Jude";
+            document.getElementById("idade-animal").textContent = "3 anos";
+            document.getElementById("sexo-animal").textContent = "Macho";
+            break;
+    }
+}
+
+function adotar() {
+    alert("Adoção realizada com sucesso!");
+}
